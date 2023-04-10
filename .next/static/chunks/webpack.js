faa7ab9dbb2c4dbe8234a46cdbf329ff5d2a0cde
BroskiVankis <ivanov.vankis@gmail.com>
2023-04-10 15:59:10 +0300

    fixed fallback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b648a6d9fcec9faf"; }
+/******/ 		__webpack_require__.h = function() { return "3e95a19e2727ffbb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
